c92a654aaae23a3f3da2475d74593d445b6c8dae
bruh045 <pattaradanai_chaitan@cmu.ac.th>
2026-02-13 20:05:12 +0700

    create app_state.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart'; // new
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart'; // new
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart'; // new
+
+import 'app_state.dart'; // new
+import 'home_page.dart';
 
 void main() {
   runApp(const MyApp());
